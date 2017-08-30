4b71154938bad7e96cb499fe83fa476f3c3e7708
Mark Hustad <hustada80@gmail.com>
2017-08-30 10:25:46 -0500

    add class component with axios library to fetch data via https request

--- index.ios.js
+++ index.ios.js
@@ -7,24 +7,47 @@
 import React from 'react';
 //import text property from react library
 // Only the root file(this file) uses 'AppRegisty'
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
-// React - Knows how a component should behave
-// knows how to take a bunch of components and make them work together
+// REACT - Knows how a component should behave
+//  knows how to take a bunch of components and make them work together
 
 // React Native - knows how to take the output from a component and place it on the screen
 // Provides default core components(image, text)
 
+// FUNCTIONAL COMPONENT
+//  - Used for presenting static data
+//  - Can't handle fetching data
+//  - Easy to write
 
+    // EX. const Header () => {
+    //   return <Text>Hi there!</Text>
+    // }
+
+//  CLASS BASED COMPONENTS - Used for dynamic sources of data
+//  - Handles any datat that might change(fetching data, user events, etc)
+//  - Knows when it gets rendered to the device(useful for data fetching)
+//  - More code to write
+
+      // EX. class Header extends Component {
+      //   render() {
+      //     return <Text>Hi There!</Text>
+      //   }
+      // }
 
 
 // Create a component
 //JS6 is an extension of JS that looks a lot like HTML. Its a mask over vanilla JS.
 
 //Whenever we want to pass some data from app to child we use the props component
+// self closing tag, pass in prop of 'headerText'
 const App = () => (
- <Header headerText={'Albums'} /> // self closing tag, pass in prop of 'headerText'
+ <View>
+  <Header headerText={'Albums'} /> 
+  <AlbumList />
+ </View>
 );
 
 
