61154c430bf5d18ee4b66e8ed0313eeb03716e09
Mark Hustad <hustada80@gmail.com>
2017-08-31 17:58:22 -0500

    display individual albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 
 //component will mount gets/loads immediately.
@@ -20,9 +21,14 @@ class AlbumList extends Component {
 	componentWillMount() {
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data })); //udpate date album state after fetching data.
 	}
+// Key property must be unique and must be the same value and must be the same across rerenders of the list.
 
 	renderAlbums() {
-		return this.state.albums.map(album => <Text>{album.title}</Text>);
+		return this.state.albums.map(album =>
+			//pass in prop album, make self closing
+			//name of variable does not have to be the same as prop, so data={album} or something.
+ 			<AlbumDetail key={album.title} album={album} />
+		);
 	}
 
 	render() {
