0ebafc2fa2e7eb39d39010f88c24d4973332303d
Mark Hustad <hustada80@gmail.com>
2017-08-31 17:44:56 -0500

    render a list of components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,27 +4,33 @@ import axios from 'axios';
 
 
 //component will mount gets/loads immediately.
-
-class AlbumList extends Component {
-	//class level property
+//class level property
 	//set initial state(empty list of albums)
 	// Rules of State
 	// - Definition of state: a plain javascript object to record and respond to user-triggered events.
 	// - When we need to update what a component shows, call `this.setState`
 	// - Only change state with `setState`, do not do `this.state = 123 or something`
 	// - Difference between prop and state(props are parent to child communication and state is for component internal record keeping)
+	// Whenver you need to reference any javascript inside JSX you use brackets{}.
+
+class AlbumList extends Component {
+
 	state = { albums: [] };
 
 	componentWillMount() {
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data })); //udpate date album state after fetching data.
 	}
 
-	render() {
+	renderAlbums() {
+		return this.state.albums.map(album => <Text>{album.title}</Text>);
+	}
 
+	render() {
 		console.log(this.state);
+
 		return (
 			<View>
-				<Text>Album List!!!!</Text>
+			{this.renderAlbums()}
 			</View>
 		);
 	}
