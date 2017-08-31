87a710cefd39eabc9448bfe5050cd031700ed42d
Mark Hustad <hustada80@gmail.com>
2017-08-31 15:07:54 -0500

    set initial state with class level component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -6,11 +6,17 @@ import axios from 'axios';
 //component will mount gets/loads immediately.
 
 class AlbumList extends Component {
+	//class level property
+	//set initial state(empty list of albums)
+	state = { albums: [] };
+
 	componentWillMount() {
-		axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data })); //udpate date album state after fetching data.
 	}
 
 	render() {
+
+		console.log(this.state);
 		return (
 			<View>
 				<Text>Album List!!!!</Text>
